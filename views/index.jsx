1691d3bc15081ddaa869b238ff26da59e98f3a09
Camilo Arguello <camilo.arguello.rincon@gmail.com>
2017-05-20 12:46:17 -0500

    Exercise 7 checked

--- views/index.jsx
+++ views/index.jsx
@@ -5,7 +5,7 @@ export default class TodoBox extends Component{
     return(
       <div className="todoBox">
         <h1>Todos</h1>
-        <TodoList />
+        <TodoList data={this.props.data} />
         <TodoForm />
       </div>
     )
@@ -14,19 +14,20 @@ export default class TodoBox extends Component{
 
 class TodoList extends Component{
   render(){
+    var todo = this.props.data.map(function (obj){
+      return (<Todo
+                title={obj.title}
+                key={obj.title}
+              >
+                {obj.detail}
+              </Todo>
+            )
+    })
     return(
       <div className="todoList">
         <table style={{border:"2px solid black"}}>
           <tbody>
-            <Todo title="Shopping">
-              Milk
-            </Todo>
-            <Todo title="Hair cut">
-              13:00
-            </Todo>
-            <Todo title="Learn React">
-            15:00
-            </Todo>
+            {todo}
           </tbody>
         </table>
       </div>
@@ -49,20 +50,23 @@ class Todo extends Component{
   render(){
     return(
       <tr>
+
         <td style={style.tableContent}>
           <input
             type="checkbox"
             checked={this.state.checked}
             onChange={this.handleChange.bind(this)}
           />
-
         </td>
+
         <td style={style.tableContent}>
           {this.props.title}
         </td>
+
         <td style={style.tableContent}>
           {this.props.children}
         </td>
+
       </tr>
     )
   }
